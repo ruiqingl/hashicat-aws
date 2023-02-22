cd5922902cff5cc2ccf7a53195a5a34932934af6
ruiqingl <39146188+ruiqingl@users.noreply.github.com>
2023-02-22 18:08:02 +1300

    Update s3-bucket.tf

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -2,7 +2,7 @@ module "s3_bucket" {
   source = "app.terraform.io/ulike/s3-bucket/aws"
   version = "2.8.0"
 
-  bucket = "my-s3-bucket"
+  # bucket = "my-s3-bucket"
   acl    = "private"
 
   versioning = {
