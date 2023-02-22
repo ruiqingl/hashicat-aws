c7f6c62e328276e1d02e527314986448c5751925
ruiqingl <rqliang@gmail.com>
2023-02-22 04:46:13 +0000

    Add S3 bucket module

---
+++ s3-bucket.tf
@@ -0,0 +1,15 @@
+module "s3_bucket" {
+  source = "terraform-aws-modules/s3-bucket/aws"
+
+  bucket = "my-s3-bucket"
+  acl    = "private"
+
+  versioning = {
+    enabled = true
+  }
+  
+  inputs = {
+    bucket_prefix = "ruiqing-liang"
+  }
+
+}
